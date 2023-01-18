17a9f48a2932f109423fe35703876ceab268d3f7
fily-gif <73085550+fily-gif@users.noreply.github.com>
2023-01-17 21:29:20 -0700

    :)
    deploying deadly neurotoxin in 3...2...1...

---
+++ back/script.js
@@ -0,0 +1,31 @@
+const greeting = document.getElementById("greeting");
+const countdown = document.getElementById("countdown");
+const birthday = new Date("January 19, 2023 00:00:00");
+
+let intervalId = setInterval(() => {
+    let now = new Date();
+    let distance = birthday - now;
+
+    if (distance < 0) {
+        clearInterval(intervalId);
+        countdown.innerHTML = "Happy Birthday!";
+        greeting.style.display = "block";
+        animateGradient();
+    } else {
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        countdown.innerHTML = `[ ${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds until the surprise is deployed :) ]`;
+    }
+}, 1000);
+
+function animateGradient() {
+    let i = 0;
+    let colors = ["orange", "purple"];
+    let intervalId = setInterval(() => {
+        document.body.style.background = `linear-gradient(to right, ${colors[i % 2]}, ${colors[(i + 1) % 2]})`;
+        i++;
+    }, 1000);
+}
